f93ae09f928221ed05ce682d8fe081eee059fe6d
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-07-17 22:55:41 +0700

    Init Xcode project

---
+++ XcresultSamples/XcresultSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcresultSamplesApp.swift
+//  XcresultSamples
+//
+//  Created by Maksim Stepanov on 17.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct XcresultSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
