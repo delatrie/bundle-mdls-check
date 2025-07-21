d65bb48cac85be1ef1266b69a33814031282ed97
Maksim Stepanov <17935127+delatrie@users.noreply.github.com>
2025-07-21 20:47:53 +0700

    fix test plans

--- XcresultSamplesSwiftTests/XcresultSamplesSwiftTests.swift
+++ XcresultSamplesSwiftTests/XcresultSamplesSwiftTests.swift
@@ -12,5 +12,9 @@ struct XcresultSamplesSwiftTests {
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
+    
+    @Test func slow() async throws {
+        try await Task.sleep(for: .seconds(1))
+    }
 
 }
